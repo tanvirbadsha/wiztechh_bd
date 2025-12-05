9fffc198686e305e4359e3e4adc1e972779fdb14
tanvirbadsha <mstanvir55@gmail.com>
2025-12-05 19:50:33 +0600

    added auto scrolling with react-fast-marquee package

--- src/components/main_layout/SellStats.jsx
+++ src/components/main_layout/SellStats.jsx
@@ -1,3 +1,4 @@
+import Marquee from "react-fast-marquee";
 import SingleStatSell from "./marketing_hero/SingleStatSell";
 
 const stats = [
@@ -20,7 +21,7 @@ const stats = [
 ];
 export default function SellStats() {
   return (
-    <div className="border border-[#505152] rounded-[40px] bg-linear-to-br from-[#2b0f26] to-[rgb(19,5,15)] p-8">
+    <div className="border border-[#505152] rounded-[40px] bg-linear-to-br from-[#2b0f26] to-[rgb(19,5,15)] p-8 relative space-y-14">
       <div className="flex justify-between items-center">
         <h1 className="font-blauerNue text-[32px] font-semibold">
           Sell from day one - even with zero audience
@@ -29,7 +30,7 @@ export default function SellStats() {
           Open Calculator
         </button>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {stats.map((stat, index) => (
           <SingleStatSell
             key={index}
@@ -39,11 +40,16 @@ export default function SellStats() {
           />
         ))}
       </div>
-      <p className="font-silika text-[20px] font-medium  bg-[#FFF3D8] text-[#695F4C]">
-        Unlike Kajabi, Teachable, or Skool, Growhubs helps you find and convert
-        clients inside the platform so you can sell from day one without paying
-        for ads.
-      </p>
+      <div className="bg-[#FFF3D8] absolute left-0">
+        <Marquee>
+          <p className="font-silika text-[20px] font-medium text-[#695F4C] px-6 py-4">
+            Unlike Kajabi, Teachable, or Skool, Growhubs helps you find and
+            convert clients inside the platform so you can sell from day one
+            without paying for ads.
+          </p>
+        </Marquee>
+      </div>
+      <div className="h-[70px]"></div>
     </div>
   );
 }
